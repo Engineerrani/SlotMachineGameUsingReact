25a25d781786a4143953c25e3e8e952b4addca91
Rani Maurya <mauryarani721@gmail.com>
2022-11-26 05:39:57 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SlotM from './SlotMach';
 
-function App() {
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <h1 className='heading_syle'>
+      🎰Welcom to <span style={{fontWeight:"bold"}}>slot machine game </span>🎰</h1>
+
+      <div className='slot_machine'>
+   
+      <SlotM x='😄' y='😄' z='😄' />
+      <SlotM x='😄' y='😄' z='😄' />
+      <SlotM x='🍎' y='🍓' z='🍏' />
+      <SlotM x='💚' y='💚' z='💚'/>
+      </div>
+    
+    </>
+  )
+};
+
 
 export default App;
